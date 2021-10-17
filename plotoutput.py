38756865a488fb20c608839cd23772b2e279994f
pulsie <pulsiedulsie@gmail.com>
2021-10-16 17:32:00 -0700

    ran again

--- plotoutput.py
+++ plotoutput.py
@@ -1,12 +1,12 @@
 #histogram from https://matplotlib.org/stable/gallery/statistics/hist.html#sphx-glr-gallery-statistics-hist-py
 import matplotlib.pyplot as plt
 import numpy as np
-with open("output.txt") as f:
+with open("output2electricboogaloo.txt") as f:
     outputstrings = f.readlines()
 stepVector=[]
 for i in outputstrings:
     stepVector.append(int(i))
 #histo from https://pythonspot.com/matplotlib-histogram/
-num_bins = 1600
+num_bins = 200
 n, bins, patches = plt.hist(stepVector, num_bins, facecolor='blue', alpha=0.5, density=False)
 plt.show()
